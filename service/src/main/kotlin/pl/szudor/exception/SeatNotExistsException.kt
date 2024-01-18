e14309a69490fdf26b168b4c1cfc0494ed5fe795
Mateusz Sidor <mateusz.sidor15@gmail.com>
2024-01-18 22:16:47 +0100

    correction of SeatNotExistsException

--- service/src/main/kotlin/pl/szudor/exception/SeatNotExistsException.kt
+++ service/src/main/kotlin/pl/szudor/exception/SeatNotExistsException.kt
@@ -2,4 +2,4 @@ package pl.szudor.exception
 
 import pl.szudor.exception.generic.NotExistsException
 
-class SeatNotExistsException(id: Int) : NotExistsException("Seating under ID: $id does not exist!")
+class SeatNotExistsException(id: Int) : NotExistsException("Seat under ID: $id does not exist!")
