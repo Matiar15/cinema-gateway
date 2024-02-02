d2c381ab530f8c5ca025f45fa125984b31993bce
Mateusz Sidor <mateusz.sidor15@gmail.com>
2024-02-02 20:09:44 +0100

    security - additions to SecurityConfig

--- war/src/main/kotlin/pl/szudor/utils/SecurityConfig.kt
+++ war/src/main/kotlin/pl/szudor/utils/SecurityConfig.kt
@@ -1,39 +1,47 @@
-package pl.szudor.security
+package pl.szudor.utils
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
 import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer.JwtConfigurer
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import pl.szudor.auth.CustomUserDetailsServiceImpl
 
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity
-class SecurityConfig {
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+class SecurityConfig(
+    private val userDetailsService: CustomUserDetailsServiceImpl,
+    private val jwtAuthorizationFilter: JwtAuthorizationFilter
+) {
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain =
         http.cors().and().csrf().disable()
             .authorizeRequests {
-                it.antMatchers("/users/**").permitAll()
+                it.antMatchers("/auth/token", "/auth").permitAll()
                 it.antMatchers(HttpMethod.GET).permitAll()
                 it.anyRequest().authenticated()
             }
-            .exceptionHandling()
-            .and()
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            .and()
+            .and().addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter::class.java)
             .build()
 
     @Bean
     fun encoder(): BCryptPasswordEncoder = BCryptPasswordEncoder()
 
     @Bean
-    fun authenticationTokenFilterBean(): Nothing = TODO("tommorow!")
+    fun authenticationManager(http: HttpSecurity): AuthenticationManager =
+        http.getSharedObject(AuthenticationManagerBuilder::class.java)
+            .userDetailsService(userDetailsService)
+            .passwordEncoder(encoder()).and()
+            .build()
+
 }
