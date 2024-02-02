9bc33946ed3b1eac3119d355c5688c26ca1dae24
Mateusz Sidor <mateusz.sidor15@gmail.com>
2024-02-02 20:07:43 +0100

    security - authentication service for tokens

---
+++ service/src/main/kotlin/pl/szudor/auth/CredentialAuthorizationServiceImpl.kt
@@ -0,0 +1,24 @@
+package pl.szudor.auth
+
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.BadCredentialsException
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.Authentication
+import org.springframework.stereotype.Service
+import pl.szudor.exception.UserNotFoundException
+
+interface CredentialAuthorizationService {
+    fun authenticate(login: String, password: String): Authentication
+}
+
+@Service
+class CredentialAuthorizationServiceImpl(
+    private val authorizationManager: AuthenticationManager,
+) : CredentialAuthorizationService {
+    override fun authenticate(username: String, password: String): Authentication =
+        try {
+            authorizationManager.authenticate(UsernamePasswordAuthenticationToken(username, password))
+        } catch (_: BadCredentialsException) {
+            throw UserNotFoundException(username)
+        }
+}
