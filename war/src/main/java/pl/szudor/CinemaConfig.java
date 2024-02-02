de108e1cce70a780072e18fa1b281f7d3bcd9475
Mateusz Sidor <mateusz.sidor15@gmail.com>
2024-02-02 20:08:34 +0100

    security - enable properties and config properties

--- war/src/main/java/pl/szudor/CinemaConfig.java
+++ war/src/main/java/pl/szudor/CinemaConfig.java
@@ -1,8 +1,12 @@
 package pl.szudor;
 
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
+@EnableConfigurationProperties
+@ConfigurationPropertiesScan
 public class CinemaConfig {
 }
