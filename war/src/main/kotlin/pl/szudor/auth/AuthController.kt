869de6401f4a6ec5c7c456c6b70fab774eb2841c
Mateusz Sidor <mateusz.sidor15@gmail.com>
2024-02-04 17:51:55 +0100

    security - moved token generation outside controller, logic is in its proper place now

--- war/src/main/kotlin/pl/szudor/auth/AuthController.kt
+++ war/src/main/kotlin/pl/szudor/auth/AuthController.kt
@@ -3,9 +3,6 @@ package pl.szudor.auth
 import org.springframework.http.HttpStatus
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
-import pl.szudor.exception.UserNotFoundException
-import pl.szudor.user.details.UserAuthority
-import pl.szudor.utils.JwtTokenManager
 import javax.validation.Valid
 
 @RestController
@@ -14,16 +11,10 @@ import javax.validation.Valid
 class AuthController(
     private val credentialAuthorizationService: CredentialAuthorizationService,
     private val customUserDetailsService: CustomUserDetailsService,
-    private val jwtTokenManager: JwtTokenManager,
 ) {
     @PostMapping("/token")
     fun createToken(@RequestBody @Valid payload: AuthPayload): AuthDto =
-        credentialAuthorizationService.authenticate(payload.username!!, payload.password!!).run {
-            val authorities = this.authorities.filterIsInstance<UserAuthority>()
-                .takeIf { it.size == this.authorities.size } ?: throw UserNotFoundException(payload.username)
-            val token = jwtTokenManager.generateToken(this.name, authorities)
-            return AuthDto(this.name, token)
-        }
+        AuthDto(payload.username!!, credentialAuthorizationService.generateToken(payload.username, payload.password!!))
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
