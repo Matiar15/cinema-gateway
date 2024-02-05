eaea3bd091ca1d4cd05cf26883aebb2e6aa1611a
Mateusz Sidor <mateusz.sidor15@gmail.com>
2024-02-05 21:11:32 +0100

    security - some corrects to CustomUserDetailsService and unit tests

--- service/src/main/kotlin/pl/szudor/auth/CustomUserDetailsServiceImpl.kt
+++ service/src/main/kotlin/pl/szudor/auth/CustomUserDetailsServiceImpl.kt
@@ -18,10 +18,11 @@ class CustomUserDetailsServiceImpl(
     private val userAuthorityRepository: UserAuthorityRepository,
     private val userAuthorityFactory: UserAuthorityFactory,
     private val userFactory: UserFactory,
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
 ) : CustomUserDetailsService {
     companion object {
         const val USER_ROLE = "USER"
+        const val CREATOR_ROLE = "CREATOR"
     }
 
     override fun createUser(username: String, password: String, email: String?): User =
@@ -29,10 +30,19 @@ class CustomUserDetailsServiceImpl(
             userRepository.saveAndFlush(
                 userFactory.createUser(
                     username,
-                    password, email
+                    password,
+                    email
                 )
             ).run {
                 userAuthorityRepository.save(userAuthorityFactory.createUserAuthority(this, USER_ROLE))
+                email?.let {
+                    userAuthorityRepository.save(
+                        userAuthorityFactory.createUserAuthority(
+                            this,
+                            CREATOR_ROLE
+                        )
+                    )
+                }
                 this
             }
         } catch (_: DataIntegrityViolationException) {
@@ -40,7 +50,10 @@ class CustomUserDetailsServiceImpl(
         }
 
     override fun addUserEmail(username: String, email: String) {
-        userRepository.requireByUsername(username).also { it.email = email }
+        userRepository.requireByUsername(username).also {
+            it.email = email
+            userAuthorityRepository.save(userAuthorityFactory.createUserAuthority(it, CREATOR_ROLE))
+        }
     }
 
     override fun loadUserByUsername(username: String): UserDetails = userRepository.requireByUsername(username)
