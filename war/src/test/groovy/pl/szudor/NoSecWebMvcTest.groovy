968fea29f8f7d25f8719ed274af284e49b7052bd
Mateusz Sidor <mateusz.sidor15@gmail.com>
2024-02-10 13:56:20 +0100

    docker - test corrects

--- war/src/test/groovy/pl/szudor/NoSecWebMvcTest.groovy
+++ war/src/test/groovy/pl/szudor/NoSecWebMvcTest.groovy
@@ -2,7 +2,10 @@ package pl.szudor
 
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.FilterType
 import org.springframework.core.annotation.AliasFor
+import pl.szudor.auth.JwtAuthorizationFilter
 
 import java.lang.annotation.Documented
 import java.lang.annotation.Inherited
@@ -12,7 +15,9 @@ import java.lang.annotation.RetentionPolicy
 @Inherited
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
-@WebMvcTest(excludeAutoConfiguration = SecurityAutoConfiguration.class)
+@WebMvcTest(excludeAutoConfiguration = SecurityAutoConfiguration.class, excludeFilters = @ComponentScan.Filter(
+        type = FilterType.ASSIGNABLE_TYPE,
+        classes = [JwtAuthorizationFilter.class]))
 @interface NoSecWebMvcTest {
 
     @AliasFor(annotation = WebMvcTest.class, attribute = "value")
