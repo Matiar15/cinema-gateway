b347b8b21f18233f3ebafa7a321fe373778d0284
Mateusz Sidor <mateusz.sidor15@gmail.com>
2024-02-10 13:56:07 +0100

    docker - application-prod.yml along with build corrections

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,42 @@
 plugins {
+    id("application")
     val kotlinVersion = "1.6.21"
+    id("org.springframework.boot") version "2.7.7"
     id("io.spring.dependency-management") version "1.1.0"
     kotlin("jvm") version kotlinVersion
     kotlin("kapt") version kotlinVersion
+    id("java")
     id("org.jetbrains.kotlin.plugin.jpa") version kotlinVersion
     id("org.jetbrains.kotlin.plugin.spring") version kotlinVersion
 }
 
+application {
+    mainClass.set("pl.szudor.CinemaApp")
+}
+
+tasks.jar {
+    manifest.attributes["Main-Class"] = "pl.szudor.CinemaApp"
+}
+
+dependencies {
+    implementation(project(mapOf("path" to ":war")))
+}
+
+dependencyManagement {
+    imports {
+        mavenBom(org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES)
+    }
+}
+
 allprojects {
     apply {
+        plugin("kotlin")
         plugin("java")
         plugin("groovy")
+    }
+
+    sourceSets.main {
+        java.srcDirs("src/main/java", "src/main/kotlin")
     }
 
     repositories {
@@ -45,6 +71,7 @@ subprojects {
         implementation("org.springframework.boot:spring-boot-starter-test:2.7.7")
         implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
         implementation("org.springframework.boot:spring-boot:2.7.7")
+        implementation("org.jetbrains.kotlin:kotlin-stdlib:1.6.21")
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.2")
         implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.15.2")
         implementation("com.mysql:mysql-connector-j:8.0.33")
